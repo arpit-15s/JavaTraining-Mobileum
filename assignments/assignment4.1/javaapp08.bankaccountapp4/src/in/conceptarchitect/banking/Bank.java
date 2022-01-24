64fbc080cb32c4299abb8ea7dc9b5c0d4e43080b
Arpit Srivastav <Arpit.Srivastav@mobileum.com>
2022-01-24 16:41:31 +0530

    assignment4.1

---
+++ assignments/assignment4.1/javaapp08.bankaccountapp4/src/in/conceptarchitect/banking/Bank.java
@@ -0,0 +1,82 @@
+package in.conceptarchitect.banking;
+
+public class Bank {
+
+	
+	private String name;
+	private double rate;
+	
+
+	public Bank(String name, double rate) {
+		// TODO Auto-generated constructor stub
+		this.name=name;
+		this.rate=rate;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	public double getRate() {
+		// TODO Auto-generated method stub
+		return rate;
+	}
+	
+	int accountCount=0, accountAdded = 0;	
+	BankAccount [] accounts=new BankAccount[100];
+
+
+	public int openAccount(String name, String password, double amount) {
+		// TODO Auto-generated method stub
+		int accountNumber= ++accountAdded;
+		accountCount++;
+		var account=new BankAccount(accountNumber, name,password,amount);
+		accounts[accountNumber]=account;
+		return accountNumber;
+	}
+	
+	public double closeAccount(int accountNumber, String password) {
+		// TODO Auto-generated method stub
+		if(accounts[accountNumber] == null) return -1;
+		if(accountNumber<1 || accountNumber>accountCount)
+			return -1;
+		
+		var account=accounts[accountNumber];
+		if(!account.authenticate(password))
+			return -1;
+		accounts[accountNumber]=null;
+		accountCount--;
+		return account.getBalance();
+	}
+
+	public int getAccountCount() {
+		// TODO Auto-generated method stub
+		return accountCount;
+	}
+
+	public BankAccount getAccount(int accountNumber, String password) {
+		// TODO Auto-generated method stub
+		
+		
+		return null;
+	}
+
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
