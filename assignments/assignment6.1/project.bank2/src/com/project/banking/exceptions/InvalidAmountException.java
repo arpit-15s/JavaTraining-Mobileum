5d7f050ac9e5672fa8914343c57a4a2bc788e7c6
Arpit Srivastav <Arpit.Srivastav@mobileum.com>
2022-01-28 09:03:16 +0530

    assignment6.1

---
+++ assignments/assignment6.1/project.bank2/src/com/project/banking/exceptions/InvalidAmountException.java
@@ -0,0 +1,40 @@
+package com.project.banking.exceptions;
+
+public class InvalidAmountException extends BankingException {
+
+	double amount;
+	public double getAmount() {return amount;}
+	
+	public InvalidAmountException(double amount) {
+		this(0,amount,"Invalid Amount Entered");
+		// TODO Auto-generated constructor stub
+	}
+	
+	public InvalidAmountException(int accountNumber,double amount) {
+		this(accountNumber,amount,"Invalid Amount Entered");
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidAmountException(int accountNumber,double amount, String message) {
+		super(accountNumber, message);
+		// TODO Auto-generated constructor stub
+		this.amount=amount;
+	}
+
+	public InvalidAmountException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidAmountException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidAmountException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
